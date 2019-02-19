187b54605628b2a53b5e8ce8925ae5836ff478b2
KylinDC <inlovingdays@gmail.com>
2019-02-19 12:44:23 +0800

    add the test of project

---
+++ src/main/java/me/model/person/Test.java
@@ -0,0 +1,42 @@
+package main.java.me.model.person;
+
+public class Test {
+
+    private Person person = new Person("1", "Tom", 21);
+
+    private Klass klass2 = new Klass(2);
+    private Klass klass3 = new Klass(3);
+
+    private Student student = new Student("2", "Li", 20, klass2);
+    private Student student2 = new Student("9", "Sun", 20, klass3);
+    private Teacher teacher = new Teacher("3", "Zhao", 34);
+
+    public void personTest() {
+        System.out.println(person.introduce());
+    }
+
+    public void studentTest() {
+        klass2.registerAssignLeaderListener(teacher);
+        klass2.registerJoinListener(teacher);
+        System.out.println(student.introduce());
+        klass2.assignLeader(student);
+        System.out.println(student.introduce());
+        klass2.assignLeader(student2);
+        klass2.appendMember(student2);
+    }
+
+    public void teacherTest() {
+        System.out.println(teacher.introduce());
+        teacher.addKlass(klass2);
+        teacher.addKlass(klass3);
+        System.out.println(teacher.introduce());
+        System.out.println(teacher.isTeaching(student));
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.personTest();
+        test.studentTest();
+        test.teacherTest();
+    }
+}
